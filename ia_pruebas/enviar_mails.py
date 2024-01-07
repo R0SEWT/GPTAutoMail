de270ad3fd020dd34007c937dd6b6f99b41cb488
R0SEWT <rody.vilchez00@gmail.com>
2024-01-06 23:53:30 -0500

    Enviar Mails aniadido

---
+++ ia_pruebas/enviar_mails.py
@@ -0,0 +1,11 @@
+# config 
+remitente = "rody.vilhcez00@gmail.com"
+destinatario = "dabat74405@regapts.com"
+mensaje = "Hola mundo"
+
+# conexion segura
+server.starttls()
+server.lo
+
+
+# enviar el correo
