844647ad1386d8632c20d02c3cd5e322f9d34b91
R0SEWT <rody.vilchez00@gmail.com>
2024-01-07 00:15:47 -0500

    psuedo main

---
+++ ia_pruebas/main.py
@@ -0,0 +1,13 @@
+import enviar_mails
+
+# cargar el prompt
+
+# cargar los usuarios (excel o json(?))
+
+# para cada usuario
+
+# personalizar el prompt
+
+# obtener respuesta del chatgpt
+
+# mandar correo
